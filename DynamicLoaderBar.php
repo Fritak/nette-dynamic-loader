44c5c34da322e68e7ad85055985262d6269af89a
Marek Sušický <susicky@rtsoft.cz>
2015-06-01 14:07:51 +0200

    Change IBarPanel.

--- DynamicLoaderBar.php
+++ DynamicLoaderBar.php
@@ -1,7 +1,9 @@
 <?php
 namespace DynamicLoader;
 
-Class DynamicLoaderBar extends \Nette\Object implements \Nette\Diagnostics\IBarPanel
+use Tracy\IBarPanel;
+
+Class DynamicLoaderBar extends \Nette\Object implements IBarPanel
 {
     public static $img = '<img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAQAAAC1+jfqAAAAAnNCSVQICFXsRgQAAAAJcEhZcwAAAG8AAABvAfGi3EMAAAAZdEVYdFNvZnR3YXJlAHd3dy5pbmtzY2FwZS5vcmeb7jwaAAAA70lEQVQYGQXBoU6VAQCA0XNn1+B0/AZQ56aBQsankCS3CEWKFjbvDLCfQuMJyGSd0ngF2XwDwI0bSLBJMNx9niNJvDZzZuaL77Y8SRKJiV3/JKNRsjBLIh44lSSjUZIcJmJXkmQ0SpJshBfuJbm06aFHtv2RX9atWeZAkguPk8SSqSXHFnb4Kck0SRKr7iQnzCUZkiQxSHLFrSQrSZJ4Jsmc35JsJUliKskPjiS58TxJPDWXZJ83FpJ8sGoweO9aknuvxEfJucGdJEnyORJ78taxJElyahJJvLNmIUny1yeTRJJYtuPElWvffPUySf4DYtnJ78i3u1cAAAAASUVORK5CYIIf02f6e8fc6151b1713c399dca89d08b4"/>';    
     public static $plugins;
